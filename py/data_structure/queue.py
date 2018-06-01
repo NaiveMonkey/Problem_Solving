9e7af8497057a7dac1e878f92bee7e909f881560
SaemiruKim <ksmr1102@gmail.com>
2018-06-02 00:22:42 +0900

    study queue

---
+++ py/data_structure/queue.py
@@ -0,0 +1,39 @@
+import unittest
+
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        self.items.pop()
+
+    def print_queue(self):
+        print(self.items)
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+class QueueTest(unittest.TestCase):
+
+    def test(self):
+        queue = Queue()
+        queue.enqueue(1)
+        self.assertEqual(queue.size(), 1)
+        queue.enqueue(2)
+        self.assertEqual(queue.size(), 2)
+        queue.print_queue()
+        queue.dequeue()
+        self.assertEqual(queue.size(), 1)
+        queue.print_queue()
+        self.assertFalse(queue.is_empty())
+
+
+if __name__ == '__main__':
+    unittest.main()
